d5ca768e6083c15cc5b5ca4c5b1b8a001e1a4391
Dennis Koja <dkoja@ceia-spa.com>
2024-03-14 16:08:04 +0100

    feat: Add JSWindow class and WindowCascadingValue component

---
+++ Blazor.Javascript.Interop/Extensions/JSRuntimeExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.JSInterop;
+
+namespace Blazor.Javascript.Interop;
+
+public static class JSRuntimeExtensions
+{
+    public static async ValueTask<JSWindow> GetWindowAsync(this IJSRuntime jsRuntime)
+    {
+        var window = await jsRuntime.GetReferenceAsync("self");
+        return new JSWindow(jsRuntime, window);
+    }
+
+    public static ValueTask<IJSObjectReference> GetReferenceAsync(this IJSRuntime jsRuntime, string identifier)
+    {
+        return jsRuntime.InvokeAsync<IJSObjectReference>("eval", $"window['{identifier}']");
+    }
+}
---
+++ Blazor.Javascript.Interop/JSWindow.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.JSInterop;
+
+namespace Blazor.Javascript.Interop;
+
+public class JSWindow(IJSRuntime jsRuntime, IJSObjectReference self)
+{
+    private readonly Lazy<JSConsole> console = new(() => new JSConsole(jsRuntime, self));
+
+    public JSConsole Console => console.Value;
+}
