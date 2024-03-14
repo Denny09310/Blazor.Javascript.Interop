74764e69034fd9d90ec6336481702750abebcd65
Dennis Koja <dkoja@ceia-spa.com>
2024-03-14 17:03:35 +0100

    feat: Add JSLocalStorage interop

---
+++ Blazor.Javascript.Interop/JSLocalStorage.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.JSInterop;
+
+namespace Blazor.Javascript.Interop;
+
+public class JSLocalStorage(IJSRuntime jsRuntime, IJSObjectReference window) : JSInteropBase(jsRuntime, window)
+{
+    public ValueTask ClearAsync() => window.InvokeVoidAsync(GetPropertyPath());
+
+    public ValueTask<T> GetItemAsync<T>(string keyName) => window.InvokeAsync<T>(GetPropertyPath(), keyName);
+
+    public ValueTask<string> KeyAsync(int index) => window.InvokeAsync<string>(GetPropertyPath(), index);
+
+    public ValueTask RemoveItemAsync(string keyName) => window.InvokeVoidAsync(GetPropertyPath(), keyName);
+
+    public ValueTask SetItemAsync<T>(string keyName, T keyValue) => window.InvokeVoidAsync(GetPropertyPath(), keyName, keyValue);
+}
