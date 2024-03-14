b39f6dab42733af9979f450c502566b4141e13c2
Dennis Koja <dkoja@ceia-spa.com>
2024-03-14 17:24:01 +0100

    feat: Add SessionStorage, Add contract for Storages

--- Blazor.Javascript.Interop/JSWindow.cs
+++ Blazor.Javascript.Interop/JSWindow.cs
@@ -4,9 +4,11 @@ namespace Blazor.Javascript.Interop;
 
 public class JSWindow(IJSRuntime jsRuntime, IJSObjectReference self)
 {
-    private readonly Lazy<JSConsole> console = new(() => new JSConsole(jsRuntime, self));
+    private readonly Lazy<JSConsole> console = new(() => new JSConsole(self));
     private readonly Lazy<JSLocalStorage> localStorage = new(() => new JSLocalStorage(jsRuntime, self));
+    private readonly Lazy<JSSessionStorage> sessionStorage = new(() => new JSSessionStorage(jsRuntime, self));
 
     public JSConsole Console => console.Value;
     public JSLocalStorage LocalStorage => localStorage.Value;
+    public JSSessionStorage SessionStorage => sessionStorage.Value;
 }
