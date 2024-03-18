31f8e0a6cbd69effcaa68645c80807809c4c3ae9
Denny09310 <k.denny2000@gmail.com>
2024-03-18 16:56:01 +0100

    feat: Add default dispose for references

--- Blazor.Javascript.Interop/Contracts/IReferenceable.cs
+++ Blazor.Javascript.Interop/Contracts/IReferenceable.cs
@@ -2,7 +2,17 @@
 
 namespace Blazor.Javascript.Interop.Contracts;
 
-public interface IReferenceable
+public interface IReferenceable : IAsyncDisposable
 {
     public IJSObjectReference Reference { get; init; }
+
+    async ValueTask IAsyncDisposable.DisposeAsync()
+    {
+        if (Reference != null)
+        {
+            await Reference.DisposeAsync();
+        }
+
+        GC.SuppressFinalize(this);
+    }
 }
