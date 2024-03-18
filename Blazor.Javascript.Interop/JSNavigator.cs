98b641c6e88c5df5b1bfdf83eee8c2002a4c28c8
Dennis Koja <dkoja@ceia-spa.com>
2024-03-18 11:01:55 +0100

    feat: Add navigator page

--- Blazor.Javascript.Interop/JSNavigator.cs
+++ Blazor.Javascript.Interop/JSNavigator.cs
@@ -7,11 +7,11 @@ public class JSNavigator(IJSObjectReference window) : JSInteropBase(window, "nav
 {
     private readonly IJSObjectReference window = window;
 
+    private IJSObjectReference? _navigator;
+    
     private Lazy<JSBluetooth>? bluetooth;
     private Lazy<JSClipboard>? clipboard;
     private Lazy<JSGeolocation>? geolocation;
-
-    private IJSObjectReference? _navigator;
 
     public JSBluetooth Bluetooth => bluetooth?.Value ?? throw new NotSupportedException("The navigator has not been initialized yet");
     public JSClipboard Clipboard => clipboard?.Value ?? throw new NotSupportedException("The navigator has not been initialized yet");
@@ -25,4 +25,16 @@ public class JSNavigator(IJSObjectReference window) : JSInteropBase(window, "nav
         clipboard = new(() => new JSClipboard(_navigator));
         geolocation = new(() => new JSGeolocation(_navigator));
     }
+
+    public ValueTask<bool> CookieEnabledAsync() => _navigator?.GetPropertyAsync<bool>("cookieEnabled") ?? throw new NotSupportedException("The navigator has not been initialized yet");
+
+    public ValueTask<string> LanguageAsync() => _navigator?.GetPropertyAsync<string>("language") ?? throw new NotSupportedException("The navigator has not been initialized yet");
+
+    public ValueTask<bool> OnlineAsync() => _navigator?.GetPropertyAsync<bool>("onLine") ?? throw new NotSupportedException("The navigator has not been initialized yet");
+
+    public ValueTask<string> PlatformAsync() => _navigator?.GetPropertyAsync<string>("appName") ?? throw new NotSupportedException("The navigator has not been initialized yet");
+
+    public ValueTask<string> UserAgentAsync() => _navigator?.GetPropertyAsync<string>("appName") ?? throw new NotSupportedException("The navigator has not been initialized yet");
+
+    public ValueTask<bool> WebDriverAsync() => _navigator?.GetPropertyAsync<bool>("webDriver") ?? throw new NotSupportedException("The navigator has not been initialized yet");
 }
