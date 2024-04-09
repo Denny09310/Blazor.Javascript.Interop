32be05ec24a0f45fdd3217dcac87c0ec1a1cd227
Denny09310 <k.denny2000@gmail.com>
2024-04-09 11:37:52 +0200

    fix: initializers not working on other templates, remove unusued function

--- Blazor.Javascript.Interop.Extensions/wwwroot/Blazor.Javascript.Interop.Extensions.lib.module.js
+++ Blazor.Javascript.Interop.Extensions/wwwroot/Blazor.Javascript.Interop.Extensions.lib.module.js
@@ -1,5 +1,8 @@
-﻿export function beforeWebStart() {
+﻿export const beforeStart = beforeStartCallback;
+export const beforeWebStart = beforeStartCallback;
+
+function beforeStartCallback() {
     var script = document.createElement('script');
-    script.setAttribute('src', './_content/Blazor.Javascript.Interop.Extensions/index.js');
-    document.head.appendChild(script);
+    script.setAttribute('src', './_content/Blazor.Javascript.Interop/index.js');
+    document.body.appendChild(script);
 }
--- Blazor.Javascript.Interop/wwwroot/Blazor.Javascript.Interop.lib.module.js
+++ Blazor.Javascript.Interop/wwwroot/Blazor.Javascript.Interop.lib.module.js
@@ -1,1 +1,8 @@
-﻿export function beforeWebStart() { }
+﻿export const beforeStart = beforeStartCallback;
+export const beforeWebStart = beforeStartCallback;
+
+function beforeStartCallback() {
+    var script = document.createElement('script');
+    script.setAttribute('src', './_content/Blazor.Javascript.Interop/index.js');
+    document.body.appendChild(script);
+}
